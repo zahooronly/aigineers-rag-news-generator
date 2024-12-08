c3589e8bfbcd8f27baa67063109c64e0a2be1592
ZahoorOnly <i.am.zahoor.a@outlook.com>
2024-12-08 22:15:04 +0500

    fix: designed Header component for the application

---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer className="border-t">
+      <div className="container mx-auto px-4 py-4 text-center">
+        <p>&copy; 2023 RAG News Generator. All rights reserved.</p>
+      </div>
+    </footer>
+  )
+}
+
